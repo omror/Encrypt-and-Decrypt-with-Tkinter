e62b744ef19c6507349741e325de9412236c7bb2
omerfarukor <omerfarukor@outlook.com>
2024-03-19 00:35:36 +0300

    encryp and decrypt added.

---
+++ encryp&decrypt.py
@@ -0,0 +1,95 @@
+from tkinter import *
+from tkinter import messagebox
+import base64
+import os
+
+def decrypt():
+    password = code.get()
+
+    if password == "1234":
+        screen2 = Toplevel(screen)
+        screen2.title("Decryption")
+        screen2.geometry("400x200")
+        screen2.configure(bg="#00bd56")
+
+        message = text1.get(1.0, END)
+        decode_message=message.encode("ascii")
+        base64_bytes = base64.b64decode(decode_message)
+        decrypt=base64_bytes.decode("ascii")
+
+        Label(screen2,text="Decrypt ", font="arial", fg="white", bg="#00bd56").place(x=10,y=0)
+        text2 = Text(screen2, font="Robote 10", bg="black", relief=GROOVE, wrap=WORD, bd=0)
+        text2.place(x=10, y=40, height=150, width=150)
+
+        text2.insert(END, decrypt)
+    elif password == "":
+        messagebox.showerror("Decryption", "Input Password")
+
+    elif password != "1234":
+        messagebox.showerror("Decryption", "Wrong Password")
+
+def encrypt():
+    password = code.get()
+
+    if password == "1234":
+        screen1 = Toplevel(screen)
+        screen1.title("Encryption")
+        screen1.geometry("400x200")
+        screen1.configure(bg="#ed3833")
+
+        message = text1.get(1.0, END)
+        encode_message=message.encode("ascii")
+        base64_bytes = base64.b64encode(encode_message)
+        encrypt=base64_bytes.decode("ascii")
+
+        Label(screen1,text="Encrypt ", font="arial", fg="white", bg="#ed3833").place(x=10,y=0)
+        text2 = Text(screen1, font="Robote 10", bg="black", relief=GROOVE, wrap=WORD, bd=0)
+        text2.place(x=10, y=40, height=150, width=150)
+
+        text2.insert(END,encrypt)
+    elif password == "":
+        messagebox.showerror("Encryption", "Input Password")
+
+    elif password != "1234":
+        messagebox.showerror("Encryption", "Wrong Password")
+
+
+def main_screen():
+
+    global screen
+    global code
+    global text1
+
+    screen = Tk()
+    screen.geometry("375x398")
+
+    #icon
+    image_icon = PhotoImage(file="img.png")
+    screen.iconphoto(False, image_icon)
+
+    def reset():
+        code.set("")
+        text1.delete(1.0, END)
+
+
+
+
+
+    screen.title("Encryption and Decryption App")
+
+    Label(text="Enter text for encryption and decryption", fg="lightblue", font = ("calibri", 13)).place(x=10,y=10)
+    text1 = Text(font="Robote 20", bg="black",relief=GROOVE,wrap=WORD,bd=0)
+    text1.place(x=10,y=50,height=100,width=355)
+
+    Label(text="Enter the key for encryption and decryption", fg="lightblue", font = ("calibri", 13)).place(x=10,y=170)
+
+    code=StringVar()
+    Entry(textvariable=code, width=19, bd=0, font=("arial", 25),show="*").place(x=10,y=200)
+
+    Button(text="Encrypt", height="2",width=23,bg="#ed3833",fg="black",bd=0,command=encrypt).place(x=10,y=250)
+    Button(text="Decrypt", height="2",width=23,bg="#00bd56",fg="black",bd=0,command=decrypt).place(x=10,y=300)
+    Button(text="Reset", height="2",width=23,bg="#f7f7f7",fg="black",bd=0,command=reset).place(x=10,y=350)
+
+    screen.mainloop()
+
+main_screen()
